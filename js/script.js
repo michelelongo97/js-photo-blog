c1cd48db2dd452bef85e2101a30bf9c41ab62d65
michelelongo97 <m.longo7x@gmail.com>
2025-01-09 17:56:21 +0100

    feat: add cards with js

---
+++ js/script.js
@@ -0,0 +1,19 @@
+const postListsElm = document.getElementById("post-lists");
+
+axios.get("https://jsonplaceholder.typicode.com/photos?_limit=6").then((res) => {
+    const posts = res.data;
+
+    posts.forEach((post) => {
+        postListsElm.innerHTML += `
+        <div class="col">
+            <div>
+                <img class="pin" src="./img/pin.svg">
+                <img class="photo" src="${post.url}"> 
+            </div>
+            <div class="text-card">
+                <p>${post.title}</p>
+            </div>
+        </div>
+        `;
+    })
+});
